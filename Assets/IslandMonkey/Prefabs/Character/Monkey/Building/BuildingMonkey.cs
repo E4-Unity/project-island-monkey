42038a33f8a3c2c73539cd8c9c9cef27992af6ad
Eu4ng <eu4ng97@gmail.com>
2023-11-22 22:48:06 +0900

    Feat : 숭숭이의 담당 건물과 휴식 건물 사이 이동 및 애니메이션 재생을 위한 기능 추가 - #10

---
+++ Assets/IslandMonkey/Prefabs/Character/Monkey/Building/BuildingMonkey.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace IslandMonkey
+{
+	public class BuildingMonkey : Monkey
+	{
+		public interface IBuilding
+		{
+			Transform Entrance { get; }
+			AnimatorOverrideController AnimatorController { get; }
+		}
+
+		enum BuildingMonkeyState
+		{
+			Init,
+			Working,
+			Resting
+		}
+
+		/* 컴포넌트 */
+		[SerializeField] Transform model;
+		NavMeshAgent agent;
+		MonkeyAnimator monkeyAnimator;
+
+		/* 필드 */
+		[SerializeField] float walkSpeedFactor = 2; // 애니메이션 속도와 실제 속도 싱크에 사용
+		[SerializeField] bool stayOnly;
+
+		IBuilding building;
+		IBuilding targetBuilding;
+		Coroutine checkRemainingDistanceCoroutine;
+		BuildingMonkeyState state = BuildingMonkeyState.Init;
+
+		/* 이벤트 */
+		public event Action OnArrived;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			agent = GetComponent<NavMeshAgent>();
+			monkeyAnimator = GetComponent<MonkeyAnimator>();
+
+			if (monkeyAnimator is not null)
+			{
+				OnArrived += () =>
+				{
+					monkeyAnimator.Stop();
+					monkeyAnimator.ResetAnimationSpeed();
+					monkeyAnimator.PlayBuildingIn();
+
+					// TODO 다른 방법?
+					var t = transform;
+					t.position = targetBuilding.Entrance.position;
+					t.rotation = targetBuilding.Entrance.rotation;
+				};
+			}
+		}
+
+#if UNITY_EDITOR
+		/* TODO 테스트용 나중에 삭제 시작 */
+		[Header("Test")]
+		[SerializeField] GameObject workBuilding;
+
+		[ContextMenu("Test Set Building")]
+		void TestSetBuilding()
+		{
+			SetBuilding(workBuilding.GetComponent<IBuilding>());
+		}
+
+		[SerializeField] GameObject restBuilding;
+		IBuilding restBuildingInterface;
+		bool isValid = true;
+
+		[ContextMenu("Test Go To Rest")]
+		void TestGoToRest()
+		{
+			if (!isValid) return;
+
+			if (restBuildingInterface is null)
+			{
+				restBuildingInterface = restBuilding.GetComponent<IBuilding>();
+				if (restBuildingInterface is null) return;
+			}
+
+			GoToRest(restBuildingInterface);
+		}
+		/* TODO 테스트용 나중에 삭제 끝 */
+#endif
+
+		/* API */
+		// 초기화 시 한 번만 호출 가능
+		public void SetBuilding(IBuilding inBuilding)
+		{
+			if (building is not null) return;
+			building = inBuilding;
+			BackToWork();
+		}
+
+		public void GoToRest(IBuilding inBuilding)
+		{
+			if (stayOnly)
+			{
+#if UNITY_EDITOR
+				Debug.LogWarning("휴식 시설을 이용할 수 없는 숭숭이입니다.");
+#endif
+				return;
+			}
+
+			if (state == BuildingMonkeyState.Resting)
+			{
+#if UNITY_EDITOR
+				Debug.LogWarning("이미 쉬고 있는 중입니다.");
+#endif
+				return;
+			}
+
+			targetBuilding = inBuilding;
+			state = BuildingMonkeyState.Resting;
+			monkeyAnimator.PlayBuildingOut();
+		}
+
+		[ContextMenu("Back To Work")]
+		public void BackToWork()
+		{
+			if (state == BuildingMonkeyState.Working)
+			{
+#if UNITY_EDITOR
+				Debug.LogWarning("이미 일하고 있는 중입니다.");
+#endif
+				return;
+			}
+
+			targetBuilding = building;
+			state = BuildingMonkeyState.Working;
+
+			if(monkeyAnimator.State == MonkeyAnimator.AnimationState.Building)
+				monkeyAnimator.PlayBuildingOut();
+			else
+				EnterBuilding(building);
+		}
+
+		/* StateMachineBehaviour 전용 */
+		// 루트 모션으로 인한 Offset 초기화
+		public void ResetModelTransform()
+		{
+			monkeyAnimator.DisableRootMotion(true);
+			model.localPosition = Vector3.zero;
+			model.localRotation = Quaternion.identity;
+		}
+
+		public void OnBuildingOutStateExit()
+		{
+			EnterBuilding(targetBuilding);
+		}
+
+		/* Method */
+		void EnterBuilding(IBuilding inBuilding)
+		{
+			monkeyAnimator.SetAnimatorController(inBuilding.AnimatorController);
+			MoveToGoal(inBuilding.Entrance);
+		}
+
+		void MoveToGoal(Transform goal)
+		{
+			if (stayOnly || agent is null) return;
+
+			agent.SetDestination(goal.position);
+
+			if (monkeyAnimator)
+			{
+				monkeyAnimator.Walk();
+				monkeyAnimator.SyncAnimationWithSpeed(agent.speed, walkSpeedFactor);
+			}
+
+			checkRemainingDistanceCoroutine ??= StartCoroutine(CheckRemainingDistance());
+		}
+
+		IEnumerator CheckRemainingDistance()
+		{
+			// SetDestination 를 호출한 직후에는 agent.remainingDistance 값이 아직 변하지 않은 상태이다.
+			yield return null;
+
+			while (agent.remainingDistance > agent.stoppingDistance)
+			{
+				yield return null;
+			}
+
+			OnArrived?.Invoke();
+			checkRemainingDistanceCoroutine = null;
+		}
+	}
+}
---
+++ Assets/IslandMonkey/Prefabs/Character/Monkey/Building/SMB_MonkeyExitBuilding.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace IslandMonkey.SMB
+{
+	public class SMB_MonkeyExitBuilding : StateMachineBehaviour
+	{
+		public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+		{
+			base.OnStateExit(animator, stateInfo, layerIndex, controller);
+
+			var monkey = animator.GetComponentInParent<BuildingMonkey>();
+			if (monkey is null) return;
+
+			monkey.ResetModelTransform();
+			monkey.OnBuildingOutStateExit();
+		}
+	}
+}
