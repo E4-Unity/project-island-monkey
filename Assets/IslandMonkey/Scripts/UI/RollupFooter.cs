2c3b7474a2a96d02c4776cc498e10bc3fb2e284c
ChoHajin <95302018+ChoHajin@users.noreply.github.com>
2023-11-25 01:58:02 +0900

    Feat : 몽키뱅크 UI 갱신 기능 및 rollupBtn 개선

--- Assets/IslandMonkey/Scripts/UI/RollupFooter.cs
+++ Assets/IslandMonkey/Scripts/UI/RollupFooter.cs
@@ -6,7 +6,9 @@ public class RollupFooter : MonoBehaviour
 {
 	public Button rollupBtn;
 	public RectTransform footer; // RectTransform으로 변경
-	public float targetY = -550f; // Footer가 내려갈 Y 좌표
+	public float targetY = -800f; // Footer가 내려갈 Y 좌표
+	public RectTransform rollupButtonRectTransform; // 추가: 버튼의 RectTransform
+	public float buttonTargetY = -600f; // 추가: 버튼이 내려갈 Y 좌표
 	public float duration = 0.5f; // 움직임의 지속 시간
 
 	private void Start()
@@ -20,5 +22,10 @@ public class RollupFooter : MonoBehaviour
 		// 현재 위치가 아니라 anchoredPosition을 기준으로 이동합니다.
 		float newY = footer.anchoredPosition.y + (footer.anchoredPosition.y == 0 ? targetY : -targetY);
 		footer.DOAnchorPosY(newY, duration);
+
+		// 버튼 이동 - 추가
+		float newButtonY = rollupButtonRectTransform.anchoredPosition.y -
+		                   (rollupButtonRectTransform.anchoredPosition.y == 0 ? buttonTargetY : -buttonTargetY);
+		rollupButtonRectTransform.DOAnchorPosY(newButtonY, duration);
 	}
 }
