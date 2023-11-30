f370e1ed3cebbd747997e9c3a30bc34a3d57e0a4
Eun-seo <58820215+Eun-seo@users.noreply.github.com>
2023-11-30 20:11:25 +0900

    Chore : 스카이박스 변경, 보관함 팝업 추가

--- Assets/IslandMonkey/Scripts/UI/Voyage/VoyageRewardPopup.cs
+++ Assets/IslandMonkey/Scripts/UI/Voyage/VoyageRewardPopup.cs
@@ -1,21 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using IslandMonkey;
+using DG.Tweening;
 
 public class VoyageRewardPopup : Popup
 {
 	[SerializeField] Button getButton;
 	[SerializeField] SceneLoadingManagerInterface sceneLoadingManagerInterface;
+	[SerializeField] Transform boat;
 
 	VoyageDataManager voyageDataManager;
 	public override void Initialize()
 	{
 		voyageDataManager = GlobalGameManager.Instance.GetVoyageDataManager();
 		getButton.onClick.AddListener(() => {
+			boat.DOMoveZ(30,30);
 			voyageDataManager.ShouldBuild = true;
 			sceneLoadingManagerInterface.GoToMainScene();
+			this.gameObject.SetActive(false);
 			});
 	}
 }
---
+++ Assets/IslandMonkey/Scripts/UI/Voyage/VoyageTopbar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoyageTopbar : MonoBehaviour
+{
+	[SerializeField] Button storageButton;
+    // Start is called before the first frame update
+    void Awake()
+    {
+		storageButton.onClick.AddListener(() => VoyageUIManager.Show<StoragePopup>(true));
+    }
+}
